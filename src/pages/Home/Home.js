829912a4927bd7d9e3530905ad189e2cd6c03e8b
CarlaSablic95 <carlasablic95@gmail.com>
2023-11-01 21:27:20 -0300

    Mejoras de estilos generales

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -77,24 +77,27 @@ const Home = () => {
             </section>
             <section id="testimonios" className="container">
                 <h2 className="text-center text-uppercase mb-5">Testimonios</h2>
-                <div className="d-flex justify-content-evenly mb-5">
-                    <div class="card card-testimony mb-3">
-                        <div class="card-body">
-                            <p class="card-text">Ir a <span className="fw-bolder">Dance Studio</span> es lo más divertido del mundo. La profesora es muy amable y nos enseña pasos de baile increíbles. Me encanta el ballet, es mi pasión. <br />¡Lo recomiendo a todos los niños!</p>
-                            <div class="card-footer text-center text-lg-end">Ana - estudiante</div>
+                <div className="row justify-content-evenly mb-5">
+
+                
+                    <div class="card col-10 col-md-4 mb-5 px-0 card-testimony mb-3">
+                            <div class="card-body">
+                                <p class="card-text">Ir a <span className="fw-bolder">Dance Studio</span> es lo más divertido del mundo. La profesora es muy amable y nos enseña pasos de baile increíbles. Me encanta el ballet, es mi pasión. <br />¡Lo recomiendo a todos los niños!</p>
+                                <div class="card-footer text-center text-lg-end">Ana - estudiante</div>
+                            </div>
+                            <div className="overlay"></div>
+
                         </div>
-                        <div className="overlay"></div>
 
-                    </div>
 
-                    <div class="card card-testimony mb-3">
+                    <div class="card col-10 col-md-4 mb-5 px-0 card-testimony mb-3">
                         <div class="card-body">
                             <p class="card-text">No tenía experiencia previa en baile, pero los profesores me han ayudado a descubrir una pasión que no sabía que tenía. <span className="fw-bolder">Dance Studio</span> es un lugar donde podés ser vos mismo y expresarte a través del baile.</p>
                         </div>
                         <div class="card-footer text-center text-lg-end">Lucas - estudiante</div>
                     </div>
 
-                    <div class="card card-testimony mb-3">
+                    <div class="card col-10 col-md-4 mb-5 px-0 card-testimony mb-3">
                         <div class="card-body">
                             <p class="card-text">Bailar en <span className="fw-bolder">Dance Studio</span> ha sido una parte fundamental de mi vida durante los últimos años. Las clases son desafiantes, pero también muy gratificantes. Estoy agradecida por todo lo que he aprendido.</p>
                         </div>
